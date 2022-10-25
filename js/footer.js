b5ef1bcbb61ab93fa249c141a93867644b5a803f
adrianfante <adrianfante@gmail.com>
2022-10-25 10:24:58 -0300

    se pone encabezado en archivo js y se comienza con el formulario de contacto

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+const footer = document.querySelector("footer");
+const piedepagina = `
+<div><p>Copyright © 2022. Todos los derechos reservados.</p>
+<p></p>
+</div>
+`
+footer.innerHTML = piedepagina;
+
