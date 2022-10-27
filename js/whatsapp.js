93976102d3d0a8e0996c93c7a3f1017cd055fd34
adrianfante <adrianfante@gmail.com>
2022-10-27 17:33:38 -0300

    agregamos api whatsapp

---
+++ js/whatsapp.js
@@ -0,0 +1,18 @@
+			$('#WABoton').floatingWhatsApp({
+				phone: '+5492477465740', // Número WhatsApp Business
+				popupMessage: 'Hola 👋 ¿Cómo podemos ayudarte?', // Mensaje pop up
+				message: "Hola Leandro...", // Mensaje por defecto
+				showPopup: true, // Habilita el pop up
+				headerTitle: 'WhatsApp Chat', // Título del header
+				headerColor: '#25D366', // Color del header
+				buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', // Icono WhatsApp
+				size: '72px', // Tamaño del icono
+				//backgroundColor: '#00000', // Color de fondo del botón
+				position: "right", // Posición del icono
+				avatar: 'https://www.w3schools.com/howto/img_avatar.png', // URL imagen avatar
+				avatarName: 'Leandro', // Nombre del avatar
+				avatarRole: 'Electricista', // Rol del avatar
+				//autoOpenTimeout: 3000,
+				zIndex: '99999',
+			});
+		
